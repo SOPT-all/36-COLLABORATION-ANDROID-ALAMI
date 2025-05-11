53740ed8e1ed3b4b736d42c01b0b1c77857356e4
Son Juwan <wndhks171@naver.com>
2025-05-12 00:10:54 +0900

    "[Feat/#5] Theme 초기세팅"

--- app/src/main/java/org/sopt/alami/core/designsystem/theme/Theme.kt
+++ app/src/main/java/org/sopt/alami/core/designsystem/theme/Theme.kt
@@ -1,5 +1,6 @@
 package org.sopt.alami.core.designsystem.theme
 
+import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
@@ -8,7 +9,61 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.ReadOnlyComposable
+import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowCompat
+
+object alarmiTheme {
+    val colors: alarmiColors
+        @Composable
+        @ReadOnlyComposable
+        get() = localAlarmiColorsProvider.current
+
+    val typography: alarmiTypography
+        @Composable
+        @ReadOnlyComposable
+        get() = localAlarmiTypographyProvider.current
+}
+
+@Composable
+fun ProviderAlarmiColorsAndTypography(
+    colors:alarmiColors,
+    typography: alarmiTypography,
+    content: @Composable () -> Unit
+){
+    CompositionLocalProvider(
+        localAlarmiColorsProvider provides  colors,
+        localAlarmiTypographyProvider provides  typography,
+        content = content
+    )
+
+}
+@Composable
+fun alarmiTheme(
+    content: @Composable () -> Unit
+){
+    ProviderAlarmiColorsAndTypography(
+        colors = defaultAlarmiColors,
+        typography = defaultAlarmiTypography,
+    ) {
+        val view = LocalView.current
+        if(!view.isInEditMode){
+            SideEffect {
+                (view.context as Activity).window.run{
+                    WindowCompat.getInsetsController(this,view).isAppearanceLightStatusBars =false
+                }
+            }
+            MaterialTheme(
+                content =content
+            )
+        }
+    }
+
+}
+
 
 private val DarkColorScheme = darkColorScheme(
     primary = Purple80,
@@ -37,7 +92,7 @@ fun AlamiTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
-    content: @Composable () -> Unit
+    content: @Composable () -> Unit,
 ) {
     val colorScheme = when {
         dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
