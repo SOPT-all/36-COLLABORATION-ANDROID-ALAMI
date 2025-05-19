1e1233ad3e89eae7ee1cbfc76a57cdfeded047d1
JAY <wndkdus102@naver.com>
2025-05-19 17:43:03 +0900

    [DELETE/#16] getTimestamp 삭제

--- app/src/main/java/org/sopt/alami/presentation/alarm/model/AlarmTime.kt
+++ app/src/main/java/org/sopt/alami/presentation/alarm/model/AlarmTime.kt
@@ -3,13 +3,4 @@ package org.sopt.alami.presentation.alarm.model
 data class AlarmTime(
     val hour: String,
     val minute: String
-) {
-
-    companion object{
-        fun getTimestamp(timestamp: String): AlarmTime {
-
-            val (hour, minute) = timestamp.split(":")
-            return AlarmTime(hour, minute)
-        }
-    }
-}
+)
