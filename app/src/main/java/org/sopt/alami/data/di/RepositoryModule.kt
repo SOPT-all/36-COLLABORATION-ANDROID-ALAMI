9c4545ecd63c20403ed76efb89eb3534056c3ab8
Son Juwan <wndhks171@naver.com>
2025-05-23 17:33:10 +0900

    [Feat/#20] 알람 추가 module

--- app/src/main/java/org/sopt/alami/data/di/RepositoryModule.kt
+++ app/src/main/java/org/sopt/alami/data/di/RepositoryModule.kt
@@ -1,3 +1,17 @@
-package org.sopt.alami.data.di
+package org.sopt.alami.core.di
 
-abstract class RepositoryModule
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import org.sopt.alami.data.repository.AlarmRepository
+import org.sopt.alami.data.service.AddAlarmService
+
+@Module
+@InstallIn(SingletonComponent::class)
+object RepositoryModule {
+    @Provides
+    fun provideAlarmRepository(
+        addAlarmService: AddAlarmService
+    ): AlarmRepository = AlarmRepository(addAlarmService)
+}
